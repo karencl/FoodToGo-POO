f8fdef8a81e8b64a4be0b00d41c702c3d8e487ee
karencl <69648059+karencl@users.noreply.github.com>
2020-11-10 10:45:44 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FoodToGo-POO
+//
+//  Created by Karen CL on 10/11/20.
+//  Copyright © 2020 KarenCbrs. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
